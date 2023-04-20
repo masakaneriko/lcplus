ea388dd7342118b67e0bb61b3e9db9e728640572
masa <120163128+lc-masa@users.noreply.github.com>
2023-04-20 15:05:45 +0900

    updated main.css path

--- functions.php
+++ functions.php
@@ -23,6 +23,6 @@ add_action('wp_enqueue_scripts', 'enqueue_google_fonts');
 function enqueue_child_theme_styles()
 {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/main.css', array('parent-style'), CHILD_THEME_VERSION);
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), CHILD_THEME_VERSION);
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_theme_styles');
