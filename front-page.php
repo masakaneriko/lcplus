a80020ab865f9b64d846bcec96fa757a3b3da435
lc-masa <myamanakalife6911@gmail.com>
2023-04-20 17:21:48 +0900

    updated front-page.php

--- front-page.php
+++ front-page.php
@@ -1,16 +1,24 @@
-<?php 
+<?php
+
 /**
- * index.php
+ * front-page.php
  */
 get_header(); ?>
 
 
 <main>
-  <body>
-    <h1>YUM YUM</h1>
+  <div>YUM YUM</div>
   <img style="min-height: 70vh" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReFFZ-Hq0f9ovUT9-2K4n0xb-czaY4E2DdYR_NaZY4&s" alt="">
-    
-  </body>
+  <?php
+  // Start the loop
+  while (have_posts()) :
+    the_post();
+
+    // Display the page content
+    the_content();
+
+  endwhile; // End of the loop.
+  ?>
 </main>
 
 <? get_footer(); ?>
