daa989c4a88f979aecaa982c9301c71df1aeec3b
lc-masa <myamanakalife6911@gmail.com>
2023-06-26 10:32:32 +0900

    disabled tagmanager for now

--- functions.php
+++ functions.php
@@ -46,8 +46,8 @@ function front_page_enqueue_script()
     wp_enqueue_script('gsap-text', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/TextPlugin.min.js', array(), '3.9.0', true);
     wp_enqueue_script('gsap-scroll', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js', null, null, true);
     wp_enqueue_script('gsap-script', get_stylesheet_directory_uri() . '/js/gsap.js', array('gsap', 'gsap-scroll'), CHILD_THEME_VERSION, true);
-    wp_enqueue_script( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', array(), true );
-    wp_enqueue_style( 'swiper-styles', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', array());
+    wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', array(), true);
+    wp_enqueue_style('swiper-styles', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', array());
     wp_enqueue_script('swiper-js', get_stylesheet_directory_uri() . '/js/swiper.js', array('swiper'), CHILD_THEME_VERSION, true);
     wp_enqueue_script('lottie', 'https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.9/lottie.min.js', array(), null, true);
     wp_enqueue_script('lottie-script', get_stylesheet_directory_uri() . '/js/lottie.js', array('lottie'), CHILD_THEME_VERSION, true);
@@ -65,11 +65,39 @@ require_once(get_stylesheet_directory() . '/shortcodes/plus-divider.php');
 
 
 // add scripts to service-template pages
-function enqueue_service_script() {
-  if ( is_page_template( 'service-template.php' ) ) {
+function enqueue_service_script()
+{
+  if (is_page_template('service-template.php')) {
     wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.0/gsap.min.js', array(), '3.9.0', true);
     wp_enqueue_script('gsap-scroll', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js', null, null, true);
-    wp_enqueue_script( 'service-script', get_stylesheet_directory_uri() . '/js/service.js', array('gsap', 'gsap-scroll'), CHILD_THEME_VERSION, true );
+    wp_enqueue_script('service-script', get_stylesheet_directory_uri() . '/js/service.js', array('gsap', 'gsap-scroll'), CHILD_THEME_VERSION, true);
   }
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_service_script' );
+add_action('wp_enqueue_scripts', 'enqueue_service_script');
+
+
+// enqueue google tag manager in head
+// function enqueue_google_tag_manager()
+// {
+// ?>
+//   <!-- Google Tag Manager -->
+//   <script>
+//     (function(w, d, s, l, i) {
+//       w[l] = w[l] || [];
+//       w[l].push({
+//         'gtm.start': new Date().getTime(),
+//         event: 'gtm.js'
+//       });
+//       var f = d.getElementsByTagName(s)[0],
+//         j = d.createElement(s),
+//         dl = l != 'dataLayer' ? '&l=' + l : '';
+//       j.async = true;
+//       j.src =
+//         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+//       f.parentNode.insertBefore(j, f);
+//     })(window, document, 'script', 'dataLayer', 'GTM-5VLKF7D');
+//   </script>
+//   <!-- End Google Tag Manager -->
+// <?php
+// }
+// add_action('wp_head', 'enqueue_google_tag_manager');
